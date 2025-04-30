a103dc5ef3b12ea4d98d3795ef122983200760e5
s <Samantha.Jones@stu.crc-lennox.qc.ca>
2025-04-30 10:25:48 -0400

    testing

--- Client/src/Register.jsx
+++ Client/src/Register.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+/* current register form*/
 export default function Register() {
   const [form, setForm] = useState({
     email: "",
